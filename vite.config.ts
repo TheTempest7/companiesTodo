d6c2d3b7722c52b18acb94c4a7ba7a81a65b4e09
Yegor <galax.yegor.uaz.s4@gmail.com>
2024-11-14 21:32:26 +0300

    fix adjustment apply 2.3

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+  },
   resolve: {
     alias: {
       "01-app": "/src/01-app",
